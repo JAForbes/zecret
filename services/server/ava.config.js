c1285f3ce20fdea7532de8f4b89a6facfa73d979
James Forbes <james.a.forbes@gmail.com>
2023-12-21 00:44:28 +0000

    Get ava working well with docker compose

---
+++ services/server/ava.config.js
@@ -0,0 +1,7 @@
+export default {
+	extensions: {
+		js: true,
+		// https://github.com/avajs/ava/discussions/3213#discussioncomment-7893977
+		ts: 'commonjs'
+	}
+}
---
+++ services/server/test/core.ts
@@ -0,0 +1,5 @@
+import test from 'ava'
+
+test('does it work 2', (t) => {
+	t.pass('yes hello')
+})
